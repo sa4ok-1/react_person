155bf120502ab58c6fed1489f159508eb994a792
Sa4ok <sasha.rusinka@gmail.com>
2024-06-10 17:51:14 +0300

    ggh

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,27 @@
-// export const Person = ({ person }) => ();
+export const Person = ({
+  person: { name, age, sex, partnerName, isMarried },
+}) => {
+  const MAN = 'm';
+  const WOMAN = 'f';
+
+  let marriedMF;
+
+  if (isMarried && (sex === MAN || sex === WOMAN)) {
+    marriedMF =
+      sex === MAN
+        ? `${partnerName} is my wife`
+        : `${partnerName} is my husband`;
+  } else {
+    marriedMF = 'I am not married';
+  }
+
+  return (
+    <div className="App">
+      <section className="Person">
+        <h2 className="Person__name">{`My name is ${name}`}</h2>
+        {age && <p className="Person__age">{`I am ${age}`}</p>}
+        <p className="Person__partner">{marriedMF}</p>
+      </section>
+    </div>
+  );
+};
